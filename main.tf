106a2c01447b895e8cbfd1b90499813fd7dd3948
Shahbaz Ali <amzteam66@gmail.com>
2026-08-04 02:24:24 -0700

    Update AMI filter and owners in main.tf

--- main.tf
+++ main.tf
@@ -1,23 +1,20 @@
 data "aws_ami" "app_ami" {
   most_recent = true
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    values = ["al2023-ami-*-x86_64"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
-  owners = ["979382823631"] # Bitnami
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-
   tags = {
     Name = "HelloWorld"
   }
