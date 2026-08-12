c9c5b17dac1cf2fb276e577cd9448358ae87d32b
Shahbaz Ali <amzteam66@gmail.com>
2026-08-12 05:50:59 -0700

    fix formatting

--- main.tf
+++ main.tf
@@ -6,6 +6,7 @@ data "aws_ami" "app_ami" {
     name   = "name"
     values = ["al2023-ami-*-x86_64"]
   }
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
@@ -18,65 +19,55 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs = [
+    "us-west-2a",
+    "us-west-2b",
+    "us-west-2c"
+  ]
+
+  private_subnets = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24"
+  ]
+
+  public_subnets = [
+    "10.0.101.0/24",
+    "10.0.102.0/24",
+    "10.0.103.0/24"
+  ]
 
   enable_nat_gateway = true
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
+}
+
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  name   = "blog"
+  vpc_id = module.blog_vpc.vpc_id
+
+  ingress_rules       = ["https-443-tcp", "http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules       = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.micro"
-  vpc_security_group_ids = [aws_security_group.blog.id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
+
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
 }
-
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "Allow http and https in Allow everything out"
-  
-  vpc_id = data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "blog_https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-resource "aws_security_group_rule" "blog_everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-
-  security_group_id = aws_security_group.blog.id
-}
-
-module "blog_sg" {
-    source       = "terraform-aws-modules/security-group/aws"
-    version      = "4.13.0"
-
-    vpc_id       = module.blog_vpc.vpc_id
-    name         = "blog"
-    ingress_rules= ["https-443-tcp","http-80-tcp"]
-    ingress_cidr_blocks = ["0.0.0.0/0"]
-    egress_rules = ["all-all"]
-    egress_cidr_blocks =  ["0.0.0.0/0"]
-  }
